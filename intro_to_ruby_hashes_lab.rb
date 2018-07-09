cbc697e60c67e9c9f1b40e385202bb395d739a4c
Christine Xiang <yutingcxiang@gmail.com>
2018-07-09 21:45:06 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,30 +1,104 @@
 def new_hash
-
+  return hash = {}
 end
 
 def actor
-
+  return hash = {:name => "Dwayne The Rock Johnson"}
 end
 
 def monopoly
-	monopoly = {}
-
+	monopoly = {:railroads => {}}
+  return monopoly
 end
 
 def monopoly_with_second_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-	
+	monopoly = {:railroads => {
+	  :pieces => 4, :names => {}, :rent_in_dollars => {}
+	}}
+  return monopoly
 end
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
-		
+	monopoly = {:railroads => {
+	  :pieces => 4, :names => {}, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
+	}}
+  return monopoly
 end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
 	
 end
+
+def new_hash
+  return hash = {}
+end
+
+def actor
+  return hash = {:name => "Dwayne The Rock Johnson"}
+end
+
+def monopoly
+	monopoly = {:railroads => {}}
+  return monopoly
+end
+
+def monopoly_with_second_tier
+	monopoly = {:railroads => {
+	  :pieces => 4, :names => {}, :rent_in_dollars => {}
+	}}
+  return monopoly
+end
+
+def monopoly_with_third_tier
+	monopoly = {:railroads => {
+	  :pieces => 4, :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline => {}}, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
+	}}
+  return monopoly
+end
+
+def monopoly_with_fourth_tier
+	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+	
+end
+
+
+
+
+def new_hash
+  return hash = {}
+end
+
+def actor
+  return hash = {:name => "Dwayne The Rock Johnson"}
+end
+
+def monopoly
+	monopoly = {:railroads => {}}
+  return monopoly
+end
+
+def monopoly_with_second_tier
+	monopoly = {:railroads => {
+	  :pieces => 4, :names => {}, :rent_in_dollars => {}
+	}}
+  return monopoly
+end
+
+def monopoly_with_third_tier
+	monopoly = {:railroads => {
+	  :pieces => 4, :names => {:reading_railroad => {}, :pennsylvania_railroad => {}, :b_and_o_railroad => {}, :shortline => {}}, :rent_in_dollars => {:one_piece_owned => 25, :two_pieces_owned => 50, :three_pieces_owned => 100, :four_pieces_owned => 200}
+	}}
+  return monopoly
+end
+
+def monopoly_with_fourth_tier
+	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
+	
+end
+
+
+
 
 
 
